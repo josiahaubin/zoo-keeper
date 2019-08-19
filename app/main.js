2c973181626dacebaa48d32394ebcc7c75e6f8db
Josiah Aubin <josiahaubin@gmail.com>
2019-08-19 16:47:11 -0600

    G class

--- app/main.js
+++ app/main.js
@@ -4,6 +4,7 @@ import Cat from "./models/Cat.js";
 import Duck from "./models/Duck.js";
 import Elephant from "./models/Elephant.js";
 import Fish from "./models/Fish.js";
+import Giraffe from "./models/Griaffe.js";
 
 let ally = new Alligator("Ally", "Green", "F", 200, 60, "big");
 let brenda = new Bear({ name: "Brenda", yardDash40: "4.35 secs", color: "brown", gender: "F", id: 1 })
@@ -12,8 +13,9 @@ let cayla = new Cat(3, "Cayla", "3.2 sec", "orange-yellow", true)
 let donald = new Duck({ id: 4, name: "Donald", color: "black", gender: "M", fly: true })
 let ella = new Elephant({ id: 5, name: "Ella", color: "grey", gender: "F" })
 let frank = new Fish(6, "Frank", "Rainbow", "M", true)
+let gerald = new Giraffe({ id: 7, name: "Gerald", color: "Yellow w/ Brown Spots", gender: "M", height: "15ft", speed: "medium-slow" })
 
-console.log(ally, brenda, carson, cayla, donald, ella, frank)
+console.log(ally, brenda, carson, cayla, donald, ella, frank, gerald)
 brenda.eat("fish")
 brenda.speak("grrrrrrrr")
 carson.eat("Zebras")
---
+++ app/models/Griaffe.js
@@ -0,0 +1,10 @@
+export default class Giraffe {
+  constructor(data) {
+    this.id = data.id;
+    this.name = data.name;
+    this.gender = data.gender;
+    this.color = data.color;
+    this.height = data.height;
+    this.speed = data.speed;
+  }
+}
